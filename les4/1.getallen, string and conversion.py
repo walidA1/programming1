90fd84bb102dc7a79cf1d6064727693d7505ffd4
walid <walid.avzaly@hu.student.nl>
2018-09-24 16:24:15 +0200

    opdracht les 4

---
+++ les4/1.getallen, string and conversion.py
@@ -0,0 +1,8 @@
+cijferPROG=8
+cijferCSN=7
+cijferICOR=9
+gemiddelde=(cijferPROG+cijferCSN+cijferICOR)/3
+beloning=(cijferPROG+cijferCSN+cijferICOR)*30
+print( 'mijn gemiddelde is'+ gemiddelde+'en mijn beloningis'+ beloning)
+
+
---
+++ les4/4.3.py
@@ -0,0 +1,4 @@
+uurloon=eval(input('vul in wat je verdient per uur: '))
+aantalUur=eval(input('vul in wat t aantal uren dat je werkt: '))
+salaris=uurloon*aantalUur
+print(aantalUur+'uur werk lever'+salaris+'euro op')
