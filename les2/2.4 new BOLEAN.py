0d5ff3c788c739683672a02754dcda8266fa4b9d
walid <walid.avzaly@hu.student.nl>
2018-10-16 17:55:25 +0200

    les 3 opdrachten

---
+++ les2/2.4 new BOLEAN.py
@@ -0,0 +1,9 @@
+a=6
+b=7
+c = (a+b)/2
+voornaam = 'Walid'
+achternaam = 'Avzaly'
+mijnnaam = voornaam + ' ' + achternaam
+
+print (75>(a) and 75<(b))
+print (len(mijnnaam) == len(voornaam+achternaam))
